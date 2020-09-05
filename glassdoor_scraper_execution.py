be210f9b1b53e9b1e3dddaefe0552a13095d956d
Kim <giminsub1@gmail.com>
2020-09-06 08:06:52 +1000

    uploaded scraper and run code

---
+++ glassdoor_scraper_execution.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 23 22:24:19 2020
+
+@author: GIS
+"""
+
+import glassdoor_scraper as gs
+import pandas as pd
+
+path = "D:/Kaggle/0. Data_Science_Salaries/ds_salary_proj/chromedriver"
+
+df = gs.get_jobs("data scientist", 15, False, path, 15)
+
+#df.to_csv('glassdoor_jobs.csv', inex=False)
